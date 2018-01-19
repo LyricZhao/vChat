9241d2c7007679ab4e566b9c986706e703a263c3
LyricZhao <lyricz@yeah.net>
2018-01-19 19:35:31 +0800

    ver.0119

--- vChatServer.py
+++ vChatServer.py
@@ -11,18 +11,24 @@ import asyncore
 server_port = 12333
 server_name = 'vChat'
 
-class EndSession(Expection): pass
-class UnknownError(Expection): pass
+class EndSession(Exception): pass
+class UnknownError(Exception): pass
+
+def debug_loop():
+    while True:
+        pass
 
 class commandHandler:
 
     def handle(self, session, line): # handle a command received from a session
+        print line
+
         if not line:
             return
-        parts = line.split('', 1)
+        parts = line.split(' ', 1)
         instruction = parts[0]
         if len(parts) > 1:
-            line = parts[1]
+            line = parts[1]
         else:
             line = ''
         if instruction == 'login': # login
@@ -61,34 +67,41 @@ class Room(commandHandler): # a chatting room
     def remove(self, session): # remove an user from a room
         self.sessions.remove(session)
 
+    def m_say(self, session, line): # say something
+        self.broadcast(session.name + " " + line)
+
     def broadcast(self, line): # send a message to everyone
         for session in self.sessions:
-            session.push("message " + line)
+            session.push("message " + line + "\r\n")
 
-    def m_logout(self, line): # logout
+    def m_logout(self): # logout
         raise EndSession
 
 class loginRoom(Room):
 
     def m_login(self, session, line):
         if self.server.main_room.check_in_db(line) == True:
-            session.push("message " + ("Welcome to %s" % self.name))
+            session.name = (line.split(' '))[0]
+            session.push("message " + ("Welcome to %s" % self.server.main_room.name) + "\r\n")
             session.enter(self.server.main_room)
         else:
-            session.push("error login")
+            session.push("error login" + "\r\n")
             raise EndSession
 
-class chatSession(asynchat): # communicate with a single user
+class chatSession(async_chat): # communicate with a single user
 
     server = None
     data = None
     room = None
+    name = None
 
     def __init__(self, server, sock):
-        asynchat_chat.__init__(self, sock)
+
+        async_chat.__init__(self, sock)
         self.server = server
         self.set_terminator('\r\n')
         self.data = []
+
         self.enter(loginRoom(server)) # login
 
     def enter(self, room): # remove and put in a new room
@@ -100,7 +113,7 @@ class chatSession(asynchat): # communicate with a single user
         self.room = room
         room.add(self)
 
-    def collect_data(self, data):
+    def collect_incoming_data(self, data):
         self.data.append(data)
 
     def found_terminator(self):
@@ -108,10 +121,10 @@ class chatSession(asynchat): # communicate with a single user
         self.data = []
         try:
             self.room.handle(self, line)
-        except:
+        except EndSession:
             self.handle_close()
 
-    def close(self):
+    def handle_close(self):
         async_chat.close(self)
         self.room.remove(self)
 
@@ -125,7 +138,7 @@ class ChatServer(dispatcher):
         self.listen(5)
         self.name = name
         self.users = {}
-        self.main_room = chatRoom(self)
+        self.main_room = Room(self, "305B")
 
     def handle_accept(self):
         conn, addr = self.accept()
